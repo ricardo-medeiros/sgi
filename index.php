b1d898ab2221ea5a14868797c8c2f92c607950b0
Ricardo Medeiros <jc_rick@oi.com.br>
2016-01-24 11:07:55 -0200

    teste

--- index.php
+++ index.php
@@ -25,12 +25,14 @@
 <?php
    if(isset($_FILES['fileUpload']))
    {
-      date_default_timezone_set("Brazil/East"); //Definindo timezone padrão
+      //date_default_timezone_set("Brazil/East"); //Definindo timezone padrão
  
       $ext = strtolower(substr($_FILES['fileUpload']['name'],-4)); //Pegando extensão do arquivo
-      $new_name = date("Y.m.d-H.i.s") . $ext; //Definindo um novo nome para o arquivo
+      $new_name = 'teste' . $ext; //Definindo um novo nome para o arquivo
       $dir = 'uploads/'; //Diretório para uploads
  
       move_uploaded_file($_FILES['fileUpload']['tmp_name'], $dir.$new_name); //Fazer upload do arquivo
    }
+   $env_var = getenv('OPENSHIFT_DATA_DIR');
+   echo $env_var;
 ?>
