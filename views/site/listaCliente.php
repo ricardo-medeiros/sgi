de14fefaa2a06ba51e28b2cd31a1bbc471354799
Ricardo Medeiros <jc_rick@oi.com.br>
2016-01-31 00:00:09 -0200

    teste

--- views/site/listaCliente.php
+++ views/site/listaCliente.php
@@ -22,7 +22,7 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <script type="text/javascript">
 	var x;
-		
+	//
 	$(document).ready(function() {
 		$('#tbcliente').DataTable(
 			{
