d2a96bfe1d99494225fb60d3f285abbe0dc0ef4f
Ricardo Medeiros <jc_rick@oi.com.br>
2016-01-30 23:50:26 -0200

    teste

--- controle/login.controller.php
+++ controle/login.controller.php
@@ -1,14 +1,35 @@
+// <?php
+// 	session_start();
+// 	$_SESSION["INDEX"]  = "http://localhost:90/sgi/index.php";
+// 	$login = $_POST["j_username"];
+//     $senha = $_POST["j_password"];
+//     //fazer conexao aqui
+// 	if ($login == 'ricardo@gmail.com' && $senha == '123')
+// 	{
+// 		$_SESSION["NOME"]   = "Ricardo dos Santos Medeiros";
+// 		$_SESSION["SITE"]   = "http://localhost:90/sgi/views/site/principal.php";
+// 		$_SESSION["LOGOUT"] = "http://localhost:90/sgi/controle/logout.controller.php";
+// 		header('Location: ' .$_SESSION["SITE"]);
+// 	}
+// 	else
+// 	{
+// 		$_SESSION["erroLogin"] = "erro";
+// 		header('Location: ' .$_SESSION["INDEX"]);
+// 	}
+// ?>
+
 <?php
 	session_start();
-	$_SESSION["INDEX"]  = "http://localhost:90/sgi/index.php";
+	$_SESSION["INDEX"]  = "http://sgi-programasweb.rhcloud.com";
 	$login = $_POST["j_username"];
-    $senha = $_POST["j_password"];
-    //fazer conexao aqui
+ $senha = $_POST["j_password"];
+
+//    fazer conexao aqui
 	if ($login == 'ricardo@gmail.com' && $senha == '123')
 	{
 		$_SESSION["NOME"]   = "Ricardo dos Santos Medeiros";
-		$_SESSION["SITE"]   = "http://localhost:90/sgi/views/site/principal.php";
-		$_SESSION["LOGOUT"] = "http://localhost:90/sgi/controle/logout.controller.php";
+		$_SESSION["SITE"]   = "http://sgi-programasweb.rhcloud.com/views/site/principal.php";
+		$_SESSION["LOGOUT"] = "http://sgi-programasweb.rhcloud.com/controle/logout.controller.php";
 		header('Location: ' .$_SESSION["SITE"]);
 	}
 	else
@@ -17,24 +38,3 @@
 		header('Location: ' .$_SESSION["INDEX"]);
 	}
 ?>
-
-//<?php
-//	session_start();
-//	$_SESSION["INDEX"]  = "http://sgi-programasweb.rhcloud.com";
-//	$login = $_POST["j_username"];
-//  $senha = $_POST["j_password"];
-
-    //fazer conexao aqui
-//	if ($login == 'ricardo@gmail.com' && $senha == '123')
-//	{
-//		$_SESSION["NOME"]   = "Ricardo dos Santos Medeiros";
-//		$_SESSION["SITE"]   = "http://sgi-programasweb.rhcloud.com/views/site/principal.php";
-//		$_SESSION["LOGOUT"] = "http://sgi-programasweb.rhcloud.com/controle/logout.controller.php";
-//		header('Location: ' .$_SESSION["SITE"]);
-//	}
-//	else
-//	{
-//		$_SESSION["erroLogin"] = "erro";
-//		header('Location: ' .$_SESSION["INDEX"]);
-//	}
-//?>
