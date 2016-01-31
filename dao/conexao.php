74a3c44027fdc6693382e686f55ed41db0b62a72
Ricardo Medeiros <jc_rick@oi.com.br>
2016-01-31 00:05:14 -0200

    teste

--- dao/conexao.php
+++ dao/conexao.php
@@ -15,24 +15,25 @@ class conexao{
 	public function conectar () {
 
 //configuração Open Shift
-// 		 $dbusername =  "adminudkGdl1";  // Mysql nome de usuário
-// 		 $dbpassword =  "prAm4Es4dwzc";  // Mysql senha
-// 		 $conn = new PDO('mysql:host=127.10.128.2:3306;dbname=sgi', $dbusername, $dbpassword);
-// 		 if (!$conn) {
-// 		 	echo "Erro ao conectar ao banco de dado SGI-MySQL.";
-// 		 	exit;
-// 		 };
+		 $dbusername =  "adminudkGdl1";  // Mysql nome de usuário
+		 $dbpassword =  "prAm4Es4dwzc";  // Mysql senha
+		 $conn = new PDO('mysql:host=127.10.128.2:3306;dbname=sgi', $dbusername, $dbpassword);
+		 if (!$conn) {
+		 	echo "Erro ao conectar ao banco de dado SGI-MySQL.";
+		 	exit;
+ 		 };
+ 		 return $conn;
 		 
 //configuração locahost
 		
-		 $dbusername =  "root";  // Mysql nome de usuário
-		 $dbpassword =  "root";  // Mysql senha
-		 $conn = new PDO('mysql:host=127.0.0.1:3306;dbname=sgi', $dbusername, $dbpassword);
-		 if (!$conn) {
-		 	echo "Erro ao conectar ao banco de dado SGI-MySQL.";
-		 	 exit;
-		 };
-		 return $conn;
+// 		 $dbusername =  "root";  // Mysql nome de usuário
+// 		 $dbpassword =  "root";  // Mysql senha
+// 		 $conn = new PDO('mysql:host=127.0.0.1:3306;dbname=sgi', $dbusername, $dbpassword);
+// 		 if (!$conn) {
+// 		 	echo "Erro ao conectar ao banco de dado SGI-MySQL.";
+// 		 	 exit;
+// 		 };
+// 		 return $conn;
 	}
 
 	public function desconectar()
